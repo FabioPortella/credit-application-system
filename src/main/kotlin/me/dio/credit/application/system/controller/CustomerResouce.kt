3b33d24a30ab588255dce5b2b1dce15f404e11cd
FabioPortella <fabioportella2009@gmail.com>
2024-02-09 16:17:26 -0300

    Endpoints CreditResouce findAllCustomerCredits

--- src/main/kotlin/me/dio/credit/application/system/controller/CustomerResouce.kt
+++ src/main/kotlin/me/dio/credit/application/system/controller/CustomerResouce.kt
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/customers")
-class CustomerResouce(
+class CustomerResource(
     private val customerService: CustomerService
 ) {
     @PostMapping
